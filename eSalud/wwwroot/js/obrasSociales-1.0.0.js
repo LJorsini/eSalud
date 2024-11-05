f00bd4e98b515bb719f6aa757ad9278c5945e320
Lucas Orsini <lucasorsini@gmail.com>
2024-11-05 00:35:15 -0300

    validaciones forms, imprimir y cambio de iconos

--- eSalud/wwwroot/js/obrasSociales-1.0.0.js
+++ eSalud/wwwroot/js/obrasSociales-1.0.0.js
@@ -1,3 +1,33 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
+
 function textoMayuscula(texto) {
     texto.value = texto.value.toUpperCase();
+  }
+
+  function GuardarObraSocial () {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Obra social cargada con exito",
+      showConfirmButton: false,
+      timer: 1500
+    });
   }
